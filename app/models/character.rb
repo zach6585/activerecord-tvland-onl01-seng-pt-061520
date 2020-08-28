e7605e08e379b23958163b528f05f2051c4ba0c8
zachary mines <zmines@wesleyan.edu>
2020-08-28 23:02:41 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,5 +4,9 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end 
+  
+  def build_show(a) 
+    b = Show.create(a)
+    
 
 end
