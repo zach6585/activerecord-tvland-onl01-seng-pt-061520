2f1279e6913442019d4bb135d57c3e2e932b122a
zachary mines <zmines@wesleyan.edu>
2020-08-28 22:50:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,8 @@
 class Show < ActiveRecord::Base
   has_many :characters 
-  has_many :actors, through: 
+  has_many :actors, through: :characters
+  def actors_list 
+    actors = [] 
+    self.actors.each do 
+  end 
 end
