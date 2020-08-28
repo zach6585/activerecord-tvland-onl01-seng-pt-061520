62dab44624807cfb61d6356d3ceb03edc1b7fcd4
zachary mines <zmines@wesleyan.edu>
2020-08-28 22:57:14 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,6 +5,6 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end 
   def build_show(name)
-    Show.new(name: self.show)
+    Show.new(name: self.show.name)
   end 
 end
